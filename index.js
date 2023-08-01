5750003eb03ae750f119aeaee0437839e376daf4
Flagg217 <flagg217@gmail.com>
2023-07-31 17:57:03 -0700

    connected to database

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 const inquirer = require('inquirer');
 const mysql2 = require('mysql2');
 
+//connect to database
+const db = mysql2.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'ecco',
+        database: 'employee_db'
+    });
